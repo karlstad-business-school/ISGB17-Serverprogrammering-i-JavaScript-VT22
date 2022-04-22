fad48d6a08118d3ef3738b10f41915d46a68c6fa
Pierre Sjöberg <pierre.sjoberg@kau.se>
2022-04-22 11:57:16 +0200

    F2 slutkod

---
+++ F2/Kodexempel-2/app.js
@@ -0,0 +1,29 @@
+'use strict';
+const fs = require('fs');
+
+
+replace().then(function(returvarde) {
+    console.log('Alles gut!');
+});
+
+
+
+
+
+async function replace() {
+    console.log('öppnar fil...');
+    let megaFil = await fs.readFileSync('stor-fil.txt').toString();
+    console.log('inläsning klar');
+    console.log('startar beräkningar');
+    for(let i=0; i<500;i++) {
+
+        for(let j=0; j<megaFil.length;j++) {
+            if(megaFil[j]=='a') {
+                megaFil[j]='y';
+            }
+        }
+    }
+
+    console.log('Beräkning klar!');
+    return Promise.resolve('allt gick fint');
+}
