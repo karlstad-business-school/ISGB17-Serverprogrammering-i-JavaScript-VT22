1a18c9c87931e418365c844721c0ff7a46c65044
Pierre Sjöberg <pierre.sjoberg@kau.se>
2022-05-05 12:06:41 +0200

    F5 slutkod

--- F5/app.js
+++ F5/app.js
@@ -1,1 +1,43 @@
 'use strict';
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+http.listen(3001,function() {
+    console.log('Server is now running...');
+});
+
+app.get('/', function(req,res) {
+    console.log('Vi har ett anrop!');
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/favicon.ico', function(req,res) {
+    console.log('FAV!');
+    res.sendFile(__dirname + '/favicon.ico');
+});
+
+app.use('/clientscripts', express.static(__dirname + '/clientscripts'));
+
+
+io.on('connection', (socket) => {
+    console.log('Ny användare anslöt via socket...');
+
+    socket.on('rndcol', slumpaNyFarg);
+
+
+});
+
+function slumpaNyFarg(msg) {
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+
+    io.emit('changeBackGroundColor', {
+        'red': r,
+        'green': g,
+        'blue': b
+    });
+}
+
--- F5/clientscripts/clientscript.js
+++ F5/clientscripts/clientscript.js
@@ -1,1 +1,16 @@
 'use strict';
+const socket = io();
+
+window.addEventListener('load', ()=> {
+    document.querySelector('.btn').addEventListener('click', banan);
+});
+
+function banan() {
+    socket.emit('rndcol', 'klickad...');
+}
+
+socket.on('changeBackGroundColor', function(data) {
+    let body = document.querySelector('body');
+
+    body.setAttribute('style', 'background-color: rgb(' + data.red + ',' + data.green + ',' + data.blue +');');
+});
